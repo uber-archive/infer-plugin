f66ada5686b378643ca78931269e20ca3f7edaf5
Tony Cosentini <tonyc@uber.com>
2015-12-11 13:48:33 -0800

    Initial commit.

---
+++ infer-sample/src/main/java/com/ubercab/inferplugin/InferPluginApplication.java
@@ -0,0 +1,5 @@
+package com.ubercab.inferplugin;
+
+import com.ubercab.core.app.CoreApplication;
+
+public class InferPluginApplication extends CoreApplication { }
---
+++ infer-sample/src/test/java/com/ubercab/inferplugin/LauncherActivityTest.java
@@ -0,0 +1,14 @@
+package com.ubercab.inferplugin;
+
+import com.ubercab.android.test.UberRobolectricBase;
+
+import org.junit.Test;
+
+public class LauncherActivityTest extends UberRobolectricBase {
+
+    @Test
+    public void testLaunchActivity_BlankTest() {
+        assert (true);
+    }
+
+}
