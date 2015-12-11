794975d2cf6b8aee900524f8487906c734487b25
Tony Cosentini <tonyc@uber.com>
2015-12-11 15:34:48 -0800

    Add MVC example.
    Summary: ignore infer results + add controller.
    
    Test Plan: None.
    
    Reviewers: lukesc, ryanson
    
    Reviewed By: ryanson
    
    Differential Revision: https://code.uberinternal.com/D270716

--- infer-sample/src/main/java/com/ubercab/infer_sample/MainActivity.java
+++ infer-sample/src/main/java/com/ubercab/infer_sample/MainActivity.java
@@ -1,23 +1,25 @@
 package com.ubercab.infer_sample;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.ubercab.core.app.CoreActivity;
+import com.ubercab.mvc.app.Controller;
+import com.ubercab.mvc.app.MvcActivity;
 
 import java.util.HashMap;
 
-public class MainActivity extends CoreActivity {
+public class MainActivity extends MvcActivity {
 
     // This is a nullable member that is initialized later in the lifecycle.
     @Nullable private Foo foo;
     //This is a member that was initialized onCreate, should be NonNull
     private String initializedOutsideConstructor;
-    //This is a member that was never initialized shoud be Nullable
+    //This is a member that was never initialized should be Nullable
     private String neverInitialized;
 
     public void init() {
-        initializedOutsideConstructor = "Im Not Null";
+        initializedOutsideConstructor = "I Am Not Null";
     }
 
     @Override
@@ -43,6 +45,12 @@ public class MainActivity extends CoreActivity {
         }
         FooManager fooManager = new FooManager(foo);
         fooManager.initialize();
+    }
+
+    @NonNull
+    @Override
+    protected Controller createController() {
+        return new MainController(this);
     }
 
     //This method should be annotated Nullable
---
+++ infer-sample/src/main/java/com/ubercab/infer_sample/MainController.java
@@ -0,0 +1,30 @@
+package com.ubercab.infer_sample;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.LinearLayout;
+
+import com.ubercab.mvc.app.Controller;
+import com.ubercab.mvc.app.MvcActivity;
+
+public class MainController extends Controller<LinearLayout> {
+
+    //This is a member that was initialized in onAttached, should be NonNull.
+    private String initializedOutsideConstructor;
+
+    public MainController(MvcActivity activity) {
+        super(activity);
+    }
+
+    public void init() {
+        initializedOutsideConstructor = "I Am Not Null";
+    }
+
+    @Override
+    protected void onAttached(@NonNull Context context, Bundle savedInstanceState) {
+        super.onAttached(context, savedInstanceState);
+        init();
+        initializedOutsideConstructor.length();
+    }
+}
