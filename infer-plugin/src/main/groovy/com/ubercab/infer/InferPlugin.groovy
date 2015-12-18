7b86031d87601d30a82a76ca173d1fdd74a144f2
Tony Cosentini <tonyc@uber.com>
2015-12-18 13:12:39 -0800

    Adding plug-in base.
    Test Plan: Build the project.
    
    Reviewers: ryanson
    
    Reviewed By: ryanson
    
    Differential Revision: https://code.uberinternal.com/D277081

---
+++ infer-plugin/src/main/groovy/com/ubercab/infer/InferPlugin.groovy
@@ -0,0 +1,12 @@
+package com.ubercab.infer
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project;
+
+class InferPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
